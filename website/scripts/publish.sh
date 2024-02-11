34aa782b7660d345c643901d5d0668124b078c48
josonking <wangsanmu5@163.com>
2024-02-12 00:52:32 +0800

    feat: update shell

--- website/scripts/publish.sh
+++ website/scripts/publish.sh
@@ -4,17 +4,17 @@
 # git clone https://gitee.com/mayuanwei/harmonyOS_bilibili.git /tmp/website
 
 # 进入代码目录
-cd /var/repo/harmonyOS_bilibili
+sudo cd /var/repo/harmonyOS_bilibili
 
 # 更新代码
-git pull
+sudo git pull
 
 # 进入网站代码根目录
-cd ./website
+sudo cd ./website
 
 # 执行 npm install 并等待完成，然后执行 npm run build
-npm install && npm run build
+sudo npm install && npm run build
 
 # 复制构建后的文件到目标目录并强制替换覆盖
-rsync -av --delete /var/repo/harmonyOS_bilibili/website/build/ /war/www/hm.codefe.cn/
+sudo rsync -av --delete /var/repo/harmonyOS_bilibili/website/build/ /war/www/hm.codefe.cn/
 
