cfe62c11d06fc7c4af850ef8aabe39caa2fc1aea
Joson <wangsanmu5@163.com>
2024-02-19 21:11:54 +0800

    feat: update script

--- website/scripts/publish.sh
+++ website/scripts/publish.sh
@@ -3,9 +3,6 @@
 # 拉取指定git仓库的代码
 # git clone https://gitee.com/mayuanwei/harmonyOS_bilibili.git /tmp/website
 
-# 加载 nvm
-source ~/.nvm/nvm.sh
-
 # 进入代码目录
 echo "cd /var/repo/harmonyOS_bilibili"
 cd /var/repo/harmonyOS_bilibili
@@ -20,17 +17,11 @@ echo "cd ./website"
 cd ./website
 pwd
 
-# 设置Node版本为18+
-nvm -v
-echo "nvm use 18"
-nvm use 18
-node -v
-
 # 执行 npm install 并等待完成，然后执行 npm run build
 sudo npm install && npm run build
 
 # 复制构建后的文件到目标目录并强制替换覆盖
-sudo rsync -av --delete /var/repo/harmonyOS_bilibili/website/build/ /war/www/hm.codefe.cn/
+sudo rsync -av --delete /var/repo/harmonyOS_bilibili/website/build/ /var/www/hm.codefe.cn/
 
 echo "[SUCCESS] deploy"
 
