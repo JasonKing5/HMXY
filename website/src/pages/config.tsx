65710850392db6c9b69fd3812d6b80cca8ce26d6
Joson <wangsanmu5@163.com>
2024-02-23 22:43:14 +0800

    feat: add server call

--- website/src/pages/config.tsx
+++ website/src/pages/config.tsx
@@ -1,17 +1,31 @@
 import Layout from '@theme/Layout';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 import styles from './index.module.css';
 
 export default function Home() {
-
+  const handlePublish = () => {
+    fetch('http://localhost:3001/config')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(data => {
+        console.log(data); // 输出 API 返回的数据
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  };
 
   return (
     <Layout
       title={`Config`}
       description="Config">
       <main>
-        <button>publish</button>
+        <button onClick={handlePublish}>publish</button>
       </main>
     </Layout>
   );
