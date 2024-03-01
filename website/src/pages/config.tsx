2339ab2134c7020d690c3655a6367d355467ffd1
Joson <wangsanmu5@163.com>
2024-03-01 20:55:59 +0800

    1

--- website/src/pages/config.tsx
+++ website/src/pages/config.tsx
@@ -7,7 +7,7 @@ import styles from './index.module.css';
 export default function Home() {
   const handlePublish = async () => {
     try {
-      const response = await axios.get('http://localhost:3001/config');
+      const response = await axios.get('http://112.83.140.12:3001/config');
       console.log(response.data); // 输出部署结果
     } catch (error) {
       console.error('部署失败:', error);
