fbbbdff82d3f7df7e7c59141f640d221c32e70f7
Joson <wangsanmu5@163.com>
2024-03-01 20:01:14 +0800

    feat: update server

--- website/src/pages/config.tsx
+++ website/src/pages/config.tsx
@@ -1,26 +1,17 @@
 import Layout from '@theme/Layout';
 import React, { useState } from 'react';
+import axios from 'axios';
 
 import styles from './index.module.css';
 
 export default function Home() {
-  const handlePublish = () => {
-    fetch('http://localhost:3001/config', {
-      method: 'GET',
-      referrerPolicy: 'no-referrer-when-downgrade' // 设置 referrerPolicy
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.text();
-      })
-      .then(data => {
-        console.log(data); // 输出 API 返回的数据
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
+  const handlePublish = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/config');
+      console.log(response.data); // 输出部署结果
+    } catch (error) {
+      console.error('部署失败:', error);
+    }
   };
 
   return (
