c9a31d0ef48ee85eb060ffa6cbe9332de74ab9b1
Jason <wangsanmu5@163.com>
2025-11-13 15:33:52 +0800

    feat: remove en files

--- docusaurus.config.common.ts
+++ docusaurus.config.common.ts
@@ -33,7 +33,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'zh-CN',
-    locales: ['zh-CN', 'en'],
+    locales: ['zh-CN'],
   },
 
   // scripts: [
